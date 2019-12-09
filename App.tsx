357e2a0be63f1cd978ed5c9785c51141ce755fcc
Chacko <chackop@gmail.com>
2019-12-09 16:08:57 +0000

    refactored nav to separate folder config

--- App.tsx
+++ App.tsx
@@ -1,29 +1,5 @@
 import React, { Component } from 'react';
-import { createStackNavigator, createAppContainer, createBottomTabNavigator, createDrawerNavigator } from 'react-navigation';
-import HomeScreen from './Screens/HomeScreen';
-import DetailScreen from './Screens/DetailScreen';
-import SignUpScreen from './Screens/SignUpScreen';
-
-
-const RootNavigator = createBottomTabNavigator(
-  {
-    Home: {
-      screen: HomeScreen,
-    },
-    SignUp: {
-      screen: SignUpScreen
-    },
-    Detail: {
-      screen: DetailScreen,
-    },
-  },
-  {
-    initialRouteName: 'SignUp',
-    headerMode: 'none',
-  }
-);
-
-const AppContainer = createAppContainer(RootNavigator);
+import { AppContainer } from './Navigation/RootNavigator';
 
 export default class App extends Component {
   render() {
