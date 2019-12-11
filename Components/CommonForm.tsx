06cdd2e14988ff0da198d9629c49f458f85b2129
Chacko <chackop@gmail.com>
2019-12-11 18:20:12 +0000

    added 2nd context to track login

--- Components/CommonForm.tsx
+++ Components/CommonForm.tsx
@@ -6,26 +6,12 @@ import UserInput from "./UserInput";
 
 export default class CommonForm extends Component {
 
-  state = {
-    isLoggedIn: this.props.isLoggedIn
-  }
-  static defaultProps = {
-    isLoggedIn: false,
-    children: {}
-
-  }
   static EmailInput = EmailInput;
   static PasswordInput = PasswordInput;
   static UserInput = UserInput;
 
-  componentDidMount() {
-    const { isLoggedIn } = this.state;
-    const children = React.Children.map(this.props.children, child => cloneElement(child, { isLoggedIn }));
-    this.setState({ children })
-  }
-
   render() {
-    return <Fragment>{this.state.children}</Fragment>;
+    return <Fragment>{this.props.children}</Fragment>;
   }
 }
 
