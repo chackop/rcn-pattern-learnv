d5c90ec1f05065008ed1b8755888a76ad24958b2
Chacko <chackop@gmail.com>
2019-12-11 18:13:34 +0000

    applied context api for text toggle

--- Screens/SignUpScreen.tsx
+++ Screens/SignUpScreen.tsx
@@ -1,31 +1,37 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 import styles from "../Components/Styles/AppStyle";
+
+import { EntryProvider } from '../Contexts/EntryContext';
+
 import { FancyButton } from "../Components/FancyButton";
-import CommonForm from '../Components/CommonForm';
+import { CustomView } from "../Components/CustomView";
+
+import CommonForm from "../Components/CommonForm";
+
 
 export default class SignUpScreen extends Component {
-
   render() {
     return (
       <View style={styles.container}>
-        {/* can toggle here to see action  */}
-        <CommonForm isLoggedIn={true}>
+        <CommonForm>
           <CommonForm.UserInput />
           <CommonForm.PasswordInput />
           <CommonForm.EmailInput />
         </CommonForm>
 
-        <FancyButton
-          text={"Sign Up"}
-          route={'SignUp'}
-          {...this.props} />
+        <FancyButton text={"Sign Up"} route={"SignUp"} {...this.props} />
+
+        <EntryProvider>
+          <CustomView />
+        </EntryProvider>
 
         <FancyButton
-          style={[styles.fancyButton, { backgroundColor: 'pink' }]}
+          style={[styles.fancyButton, { backgroundColor: "pink" }]}
           text={"Sign in"}
-          route={'Home'}
-          {...this.props} />
+          route={"Home"}
+          {...this.props}
+        />
       </View>
     );
   }
