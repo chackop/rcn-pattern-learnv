3d8fee4366ff302f8bb2229db7efbd7eeefb547d
Chacko <chackop@gmail.com>
2019-12-11 17:03:45 +0000

    apply secodanry common component to apply the compoun component flexibility with static input components

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -2,41 +2,17 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import styles from '../Components/Styles/AppStyle';
 import { FancyButton } from '../Components/FancyButton';
-import { FancyInput } from '../Components/FancyInput';
-import { ValidationMessage } from '../Components/ValidationMessage';
+import CommonForm from '../Components/CommonForm';
 
 export default class HomeScreen extends Component {
-  constructor() {
-    super();
-    this.state = {
-      user: '',
-      password: ''
-    };
-  }
-  setItem(key, value) {
-    this.setState({ [key]: value });
-  }
 
   render() {
     return (
       <View style={styles.container}>
-        <FancyInput
-          item={'user'}
-          value={this.state.user}
-          setItem={this.setItem.bind(this)} />
-
-        <FancyInput
-          item={'password'}
-          value={this.state.password}
-          setItem={this.setItem.bind(this)}
-          renderValidation={
-            this.state.password.length < 8
-              ? () => (
-                <ValidationMessage rule='Password must be at least 8 characters long' />
-              )
-              : null
-          }
-        />
+        <CommonForm>
+          <CommonForm.UserInput />
+          <CommonForm.PasswordInput />
+        </CommonForm>
 
         <FancyButton
           text={'Log in'}
--- Screens/SignUpScreen.tsx
+++ Screens/SignUpScreen.tsx
@@ -1,64 +1,20 @@
-import React, { Component, Fragment } from "react";
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import { View } from "react-native";
 import styles from "../Components/Styles/AppStyle";
 import { FancyButton } from "../Components/FancyButton";
-import { FancyInput } from "../Components/FancyInput";
-
-// fake state to be shared
-const showCloneConfig = {
-  display: true,
-  item: 'clonedUser',
-  value: 'showCloneConfigUser'
-};
-
-const DisplayClone = ({ showClone, children }) => (showClone.display) ? React.Children.only(children) : null;
-
-const TheClone = ({ item, value }) => (
-  <Fragment>
-    <Text> Text in clone </Text>
-    {React.cloneElement(<FancyInput />, { item, value })}
-  </Fragment>
-);
+import CommonForm from '../Components/CommonForm';
 
 export default class SignUpScreen extends Component {
-  constructor() {
-    super();
-    this.state = {
-      user: "",
-      password: "",
-      email: ""
-    };
-  }
-  setItem(key, value) {
-    this.setState({ [key]: value });
-  }
 
   render() {
-
     return (
       <View style={styles.container}>
-
-        <DisplayClone showClone={showCloneConfig}>
-          <TheClone item={showCloneConfig.item} value={showCloneConfig.value} />
-        </DisplayClone>
-
-        <FancyInput
-          item={"user"}
-          value={this.state.user}
-          setItem={this.setItem.bind(this)}
-        />
-
-        <FancyInput
-          item={"password"}
-          value={this.state.password}
-          setItem={this.setItem.bind(this)}
-        />
-
-        <FancyInput
-          item={"email"}
-          value={this.state.email}
-          setItem={this.setItem.bind(this)}
-        />
+        {/* can toggle here to see action  */}
+        <CommonForm isLoggedIn={true}>
+          <CommonForm.UserInput />
+          <CommonForm.PasswordInput />
+          <CommonForm.EmailInput />
+        </CommonForm>
 
         <FancyButton
           text={"Sign Up"}
